1776e7bdffde2bfd08db929554ff7ce6148e7bc6
uminchu <uminchu@uminchu.jp>
2024-12-02 01:02:08 +0900

    delete experimental

--- lexa/tools.py
+++ lexa/tools.py
@@ -14,7 +14,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.compat.v1 as tf1
 import tensorflow_probability as tfp
-from tensorflow.keras.mixed_precision import experimental as prec
+from tensorflow.keras import mixed_precision as prec
 from tensorflow_probability import distributions as tfd
 
 
